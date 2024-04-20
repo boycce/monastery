cd640474c4c0f54c6ea9cc67ac165d371efaec1b
Ricky Boyce <boycce@gmail.com>
2024-04-20 21:03:55 +1200

    added some monk tests

--- test/monk.js
+++ test/monk.js
@@ -37,4 +37,17 @@ module.exports = function(monastery, opendb) {
     })
   })
 
+  test('Monk basic operations', async () => {
+    let db = monastery('localhost/monastery', { serverSelectionTimeoutMS: 2000 })
+    let userCol = db.get('user')
+
+    // Insert (returns whole document)
+    let res1 = await userCol.insert({ woot: 'b' })
+    expect(res1).toEqual(expect.objectContaining({ woot: 'b', _id: expect.any(Object) }))
+
+    // Update (returns whole document)
+    let res2 = await userCol.update({ woot: 'b' }, { $set: { woot: 'c' }})
+    expect(res2).toEqual(expect.objectContaining({ n: 1, nModified: 1, ok: 1 }))
+  })
+
 }
